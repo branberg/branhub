7f6bf64b8a44930d86479b40379d03cd9ffbec91
Sam Brannon <sam@evolvelabs.com>
2015-07-18 00:01:55 -0500

    Init

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>App "branhub" set up by ServerPilot</title>
+    <meta charset="utf-8"/>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+        #header {
+            background-color: #f1f1f1;
+            border-bottom: 1px solid #ddd;
+            padding: 30px;
+            text-align: center;
+            color: #444;
+            font-size: 22px;
+        }
+        #content {
+            padding: 30px;
+            text-align: center;
+            font-size: 18px;
+        }
+        #content a {
+            color: #3e68ff;
+        }
+        #footer {
+            padding: 30px;
+            text-align: center;
+            font-size: 12px;
+            color: #888;
+        }
+        #footer a {
+            color: #555;
+        }
+    </style>
+</head>
+<body>
+<div id="header">
+    Your app "branhub" is set up.
+</div>
+<div id="content">
+    <?php echo '<p>You should replace this index.php file with your own.</p>'; ?>
+    <p><a href="https://manage.serverpilot.io">Manage this app</a></p>
+</div>
+<div id="footer">
+    Easily manage your PHP apps and servers with <a href="https://serverpilot.io">ServerPilot</a>.
+</div>
+
+</body>
+</html>
